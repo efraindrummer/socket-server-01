c7e535fb7b411ec7d153aa1377642124a9e9fbbb
Efrain May <wwifihack@gmail.com>
2020-12-15 14:53:35 -0600

    conexion de socket io a carpeta public

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //servidor de express
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 //servidor del socket
 const server = require('http').createServer(app);
@@ -7,8 +8,12 @@ const server = require('http').createServer(app);
 //configuracion del socket server
 const io = require('socket.io')(server);
 
+//desplegar el directorio public
+app.use(express.static( __dirname + '/public'));
 //conexion
-io.on('connection', () => { /* … */ });
+io.on('connection', () => {
+    console.log('cliente conectado!');
+});
 server.listen(8080, () => {
     console.log('Servidor corriendo en puerto: 8080');
 });
