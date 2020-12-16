dd841b694f9d86eaa0e5d8fbc9d390529dbb0088
Efrain May <wwifihack@gmail.com>
2020-12-16 00:55:43 -0600

    se agrego la libreria del cors

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
+const cors     = require('cors');
 const Sockets  = require('./sockets');
 
 class Server {
@@ -19,6 +20,8 @@ class Server {
     middlewares(){
         //desplegar el directorio public
         this.app.use(express.static( path.resolve( __dirname + '../../public') ));
+        //habilitar cors
+        this.app.use(cors());
     }
 
     configurarSockets(){
